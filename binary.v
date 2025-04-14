40dbb4f6bb3a009e16bc8c641c5c0f3783e9cf63
zellarun <123707677+zellarun@users.noreply.github.com>
2025-04-13 19:03:31 -0600

    Update binary.v

--- binary.v
+++ binary.v
@@ -2,33 +2,38 @@ module binary(
     input w,
     input clk,
     input reset,
-    output z
+    output z,
+    output [2:0] state_leds
 );
 
-    wire [2:0] State;
-    wire [2:0] Next;
+    wire [2:0] State, Next;
+
+    assign state_leds = State; //for the output
 
     dff zero(
         .D(Next[0]),
         .clk(clk),
-        .Q(State[0])
+        .Q(State[0]),
+        .reset(reset)
     );
 
     dff one(
         .D(Next[1]),
         .clk(clk),
-        .Q(State[1])
+        .Q(State[1]),
+        .reset(reset)
     );
     
     dff two(
         .D(Next[2]),
         .clk(clk),
-        .Q(State[2])
+        .Q(State[2]),
+        .reset(reset)
     );
 
-    assign z = (State[1] & ~w) | (~State[0] & w);
-    assign Next[0] = (~State[0] & ~State[1] & ~w) | (State[0] & State[1] & ~w) | (~State[1] & w & ~State[2]) | (~State[0] & w & ~State[2]);
-    assign Next[1] = (State[0] & ~State[1] & ~w) | (~State[0] & State[2] & ~w) | (~State[1] & ~State[2] & w);
-    assign Next[2] = (State[0] & State[1] & ~w) | (State[0] & w & State[2]);
+    assign z = (State == 3'b010) || (State == 3'b100); //z is 1 in state C or E
+    assign Next[0] = (~State[2] & ~State[1] & ~State[0] & ~w) | (~State[2] & State[1] & ~State[0] & ~w) | (State[2] & ~State[1] & ~State[0] & ~w) | (~State[2] & State[1] & State[0] & ~w);
+    assign Next[1] = (~State[2] & ~State[1] & State[0] & ~w) | (~State[2] & State[1] & ~State[0] & ~w) | (~State[2] & ~State[1] & ~State[0] & w);
+    assign Next[2] = (~State[2] & State[1] & State[0] & w) | (State[2] & ~State[1] & ~State[0] & w);
 
 endmodule
